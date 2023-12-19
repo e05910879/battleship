fa17fcb6577af5040aab0a614889f1dbc4565904
Ernest <143043338+e05910879@users.noreply.github.com>
2023-12-18 17:25:13 -0800

    Create ship object and test cases for its methods

--- Ship.js
+++ Ship.js
@@ -1,3 +1,11 @@
-export function Ship() {
-    return;
+export function Ship(length = 2) {
+    let hitCount = 0;
+    let sunk = false;
+
+    function getLength() { return length; };
+    function getHitCount() { return hitCount; };
+    function hit() { hitCount++; };
+    function isSunk() { return hitCount >= length ? true : false; }
+
+    return { getLength, getHitCount, hit, isSunk };
 }
--- battleship.test.js
+++ battleship.test.js
@@ -1,5 +1,34 @@
 import { Ship } from './Ship.js';
 
-test('Ship: ship exists.', () => {
-    expect(Ship()).toBeFalsy();
+test('Ship: function returns an object.', () => {
+    expect(Ship()).not.toBeFalsy();
 });
+
+test('Ship: object returned contains methods for length, number of times hit, and sunk status.', () => {
+    expect(Ship()).toHaveProperty('getLength', 'getHitCount', 'getSunkStatus');
+});
+
+test('Ship: function invocation results in initialized values.', () => {
+    let ship = Ship();
+    expect(ship.getLength()).toBe(2);
+    expect(ship.getHitCount()).toBe(0);
+    expect(ship.isSunk()).toBe(false);
+})
+
+test('Ship: hit() function increases the number of hits in ship.', () => {
+    let ship = Ship();
+    expect(ship.getHitCount()).toBe(0);
+    ship.hit();
+    expect(ship.getHitCount()).toBe(1);
+});
+
+test('Ship: isSunk() returns true if the ship\'s length equals its number of hits.', () => {
+    let ship = Ship();
+    expect(ship.isSunk()).toBe(false);
+
+    ship.hit();
+    expect(ship.isSunk()).toBe(false);
+
+    ship.hit();
+    expect(ship.isSunk()).toBe(true);
+})
